8f8f1b1b13436826f8f00d32fbbd73c6f592a385
Rupesh <srupesh51@gmail.com>
2018-03-18 12:50:25 -0600

    Added prompt for select_account instead of consent

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,7 +5,7 @@ Rails.application.middleware.use OmniAuth::Builder do
    provider_name = "google_oauth2"
    provider "#{provider_name}", ENV["#{provider_name.upcase}_API_KEY"],
    ENV["#{provider_name.upcase}_API_SECRET"], {
-      prompt: 'consent',
+      prompt: 'select_account',
       scope: 'email https://www.google.com/m8/feeds/',
       ssl: {ca_file: Rails.root.join("cacert.pem").to_s}
   }
